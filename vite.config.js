ee527858d66b55122db33be540ce745547690dc4
Manuel Gatica <manuel.gatica.holtmann@gmail.com>
2026-04-01 15:21:34 -0300

    feat: ofuscación de código en build, repo privado

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import obfuscatorPlugin from 'rollup-plugin-obfuscator'
 
 export default defineConfig({
   base: '/pinguinocracia2/',
@@ -6,6 +7,25 @@ export default defineConfig({
     port: 3000
   },
   build: {
-    target: 'es2020'
+    target: 'es2020',
+    rollupOptions: {
+      plugins: [
+        obfuscatorPlugin({
+          options: {
+            compact: true,
+            controlFlowFlattening: true,
+            controlFlowFlatteningThreshold: 0.5,
+            deadCodeInjection: true,
+            deadCodeInjectionThreshold: 0.2,
+            stringArray: true,
+            stringArrayThreshold: 0.5,
+            stringArrayEncoding: ['base64'],
+            renameGlobals: false,
+            selfDefending: false,
+            identifierNamesGenerator: 'hexadecimal'
+          }
+        })
+      ]
+    }
   }
 })
